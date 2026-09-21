9c3428fec58d1992e216cf3c99fa9ad4f9d83173
shaber-717 <shalva@talorim.com>
2026-09-21 21:31:39 +0400

    Delete bog_connector directory

--- bog_connector/banking/doctype/bog_sync_log/bog_sync_log.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Talorim and contributors
-# For license information, please see license.txt
-
-import frappe
-from frappe.model.document import Document
-
-
-class BOGSyncLog(Document):
-	pass
